0473bf094991fdf6eae0e3a9325456400ceb4fb7
Jonathan Londoño <jonathanlon.66@gmail.com>
2025-07-04 23:50:23 -0500

    Tu mensaje de commit describiendo los cambios

--- terraform-skillnest/terraform-api-ecs/modules/alb/outputs.tf
+++ terraform-skillnest/terraform-api-ecs/modules/alb/outputs.tf
@@ -10,5 +10,5 @@ output "security_group_id" {
 
 output "listener_arn" {
   description = "ARN of the ALB listener"
-  value       = aws_lb_listener.http.arn
+  value       = data.aws_lb_listener.http.arn
 }
